d58421179b2027e0d497bd51cf27abb01b93c30d
stevenyu253436 <stevenyu.supreme@gmail.com>
2024-09-23 12:28:21 +0800

    Add HelpView to SwiftiDate app with multiple sections including overview, chat, and premium features

---
+++ SwiftiDate/HelpView.swift
@@ -0,0 +1,172 @@
+//
+//  HelpView.swift
+//  SwiftiDate
+//
+//  Created by 游哲維 on 2024/9/23.
+//
+
+import Foundation
+import SwiftUI
+
+struct HelpView: View {
+    @Binding var isHelpView: Bool // Binding to control the dismissal of HelpView
+    
+    // Extracting the data to separate properties
+    private let overviewTopics = ["SwiftiDate 是什麼？", "SwiftiDate 需要付費使用嗎？"]
+    private let chatTopics = [
+        "如何與他人配對、聊天？",
+        "我能查看誰喜歡了我嗎？",
+        "為什麼我很難配對成功？",
+        "如何解除配對？",
+        "可以刪除傳出去的訊息嗎？",
+        "如何檢舉用戶？"
+    ]
+    private let premiumTopics = [
+        "什麼是 SwiftiDate Premium？",
+        "如何使用「看看誰喜歡你」與「開啟心動列表」功能？",
+        "「Crush」是什麼？如何使用？",
+        "「Turbo」是什麼？如何使用？",
+        "滑錯如何反悔？",
+        "如何開啟 Premium 專屬標誌？",
+        "如何購買 SwiftiDate Premium？",
+        "如何取消自動續費？",
+        "支付完成後，特權未生效怎麼辦？",
+        "如果已經成功續訂 SwiftiDate Premium，還可以退款嗎？",
+        "SwiftiDate Premium 的價格有優惠嗎？"
+    ]
+    
+    private let supremeTopics = [
+        "什麼是 SwiftiDate Supreme？",
+        "如何使用「每日 3 次的讚美機會」功能？",
+        "如何使用「高級篩選」功能？",
+        "如何使用「隱身模式」功能？",
+        "如何購買 SwiftiDate Supreme？"
+    ]
+    
+    private let personalInfoTopics = [
+        "如何編輯我的個人資料？",
+        "如何更改我的照片？",
+        "我需要上傳什麼樣的照片作為頭像？",
+        "如何通過真人認證？",
+        "如何更改我的搜尋偏好？",
+        "如何修改備註名稱？",
+        "如何搜尋配對好友？",
+        "如何收到SwiftiDate的訊息提醒？",
+        "如何註銷我的帳號"
+    ]
+    
+    var body: some View {
+        VStack {
+            // Custom Navigation Bar
+            HStack {
+                Button(action: {
+                    isHelpView = false // Dismiss HelpView and return to SettingsView
+                }) {
+                    Image(systemName: "chevron.left")
+                        .font(.title)
+                        .foregroundColor(.blue)
+                }
+                
+                Text("SwiftiDate 幫助")
+                    .font(.headline)
+                    .padding(.leading, 5)
+                
+                Spacer()
+            }
+            .padding()
+            
+            Divider()
+            
+            // Help content
+            List {
+                Section(header: Text("SwiftiDate 概覽")
+                            .font(.subheadline)
+                            .foregroundColor(.gray)
+                            .padding(.vertical, 5)) {
+                    ForEach(overviewTopics, id: \.self) { item in
+                        HStack {
+                            Text(item)
+                                .padding(.vertical, 10)
+                            Spacer()
+                            Image(systemName: "chevron.right")
+                                .foregroundColor(.gray)
+                        }
+                    }
+                }
+                
+                Section(header: Text("配對與聊天")
+                            .font(.subheadline)
+                            .foregroundColor(.gray)
+                            .padding(.vertical, 5)) {
+                    ForEach(chatTopics, id: \.self) { topic in
+                        HStack {
+                            Text(topic)
+                                .padding(.vertical, 10)
+                            Spacer()
+                            Image(systemName: "chevron.right")
+                                .foregroundColor(.gray)
+                        }
+                    }
+                }
+                
+                // Adding the SwiftiDate Premium section
+                Section(header: Text("SwiftiDate Premium")
+                            .font(.subheadline)
+                            .foregroundColor(.gray)
+                            .padding(.vertical, 5)) {
+                    ForEach(premiumTopics, id: \.self) { topic in
+                        HStack {
+                            Text(topic)
+                                .padding(.vertical, 10)
+                            Spacer()
+                            Image(systemName: "chevron.right")
+                                .foregroundColor(.gray)
+                        }
+                    }
+                }
+                
+                // Adding the SwiftiDate Supreme section
+                Section(header: Text("SwiftiDate Supreme")
+                            .font(.subheadline)
+                            .foregroundColor(.gray)
+                            .padding(.vertical, 5)) {
+                    ForEach(supremeTopics, id: \.self) { topic in
+                        HStack {
+                            Text(topic)
+                                .padding(.vertical, 10)
+                            Spacer()
+                            Image(systemName: "chevron.right")
+                                .foregroundColor(.gray)
+                        }
+                    }
+                }
+                
+                // Adding the Personal Information and Settings section
+                Section(header: Text("個人資訊與設定")
+                            .font(.subheadline)
+                            .foregroundColor(.gray)
+                            .padding(.vertical, 5)) {
+                    ForEach(personalInfoTopics, id: \.self) { topic in
+                        HStack {
+                            Text(topic)
+                                .padding(.vertical, 10)
+                            Spacer()
+                            Image(systemName: "chevron.right")
+                                .foregroundColor(.gray)
+                        }
+                    }
+                }
+            }
+            .listStyle(GroupedListStyle()) // To create the grouped appearance like in your image
+        }
+    }
+}
+
+// Preview for HelpView
+struct HelpView_Previews: PreviewProvider {
+    @State static var isHelpView = true // Provide a sample state variable for the preview
+
+    static var previews: some View {
+        HelpView(isHelpView: $isHelpView) // Use the binding variable in the preview
+    }
+}
--- SwiftiDate/SettingsView.swift
+++ SwiftiDate/SettingsView.swift
@@ -10,83 +10,196 @@ import SwiftUI
 
 struct SettingsView: View {
     @Binding var showSettingsView: Bool // Binding variable to control the view dismissal
+    @State private var isHelpView = false // State variable to control HelpView presentation
 
     var body: some View {
-        VStack {
-            // Custom Navigation Bar
-            HStack {
-                Button(action: {
-                    showSettingsView = false // Dismiss SettingsView and return to ProfileView
-                }) {
-                    Image(systemName: "chevron.left")
-                        .font(.title)
-                        .foregroundColor(.blue)
+        if isHelpView {
+            HelpView(isHelpView: $isHelpView) // Use the binding variable in the preview
+        } else {
+            VStack {
+                // Custom Navigation Bar
+                HStack {
+                    Button(action: {
+                        showSettingsView = false // Dismiss SettingsView and return to ProfileView
+                    }) {
+                        Image(systemName: "chevron.left")
+                            .font(.title)
+                    }
+                    Text("設定")
+                        .font(.headline)
+                        .padding(.leading, 5)
+                    
+                    Spacer()
                 }
-                Text("設定")
-                    .font(.headline)
-                    .padding(.leading, 5)
+                .padding()
                 
-                Spacer()
-            }
-            .padding()
-            
-            Divider()
-            
-            // Settings content
-            List {
-                Section {
-                    HStack {
-                        Image(systemName: "qrcode.viewfinder")
-                            .foregroundColor(.purple)
-                        Text("掃碼")
-                        Spacer()
-                    }
-                    
-                    HStack {
-                        Image(systemName: "crown.fill")
-                            .foregroundColor(.orange)
-                        Text("恢復購買")
-                        Spacer()
-                    }
-                    
-                    HStack {
-                        Image(systemName: "crown")
-                            .foregroundColor(.brown)
-                        Text("開啟 SwiftiDate Supreme 會員標誌")
-                        Spacer()
-                        Toggle("", isOn: .constant(true))
-                            .labelsHidden()
-                    }
-                    
-                    HStack {
-                        Image(systemName: "envelope.fill")
-                            .foregroundColor(.pink)
-                        Text("邀請好友一起玩SwiftiDate")
-                        Spacer()
-                    }
-                    
-                    HStack {
-                        Image(systemName: "crown.fill")
-                            .foregroundColor(.pink)
-                        Text("兌換會員")
-                        Spacer()
-                    }
-                    
-                    HStack {
-                        Image(systemName: "person.fill")
-                            .foregroundColor(.cyan)
-                        Text("個人資料")
-                        Spacer()
-                    }
-                }
+                Divider()
                 
-                Section {
-                    Text("客服")
-                    Text("幫助")
-                    Text("語言")
-                    Text("社區規範")
+                // Settings content
+                List {
+                    Section {
+                        HStack {
+                            Image(systemName: "qrcode.viewfinder")
+                                .foregroundColor(.purple)
+                            Text("掃碼")
+                            Spacer()
+                        }
+                        .padding(.vertical, 10) // Adjust this value to increase the height
+                        
+                        HStack {
+                            Image(systemName: "crown.fill")
+                                .foregroundColor(.orange)
+                            Text("恢復購買")
+                            Spacer()
+                        }
+                        .padding(.vertical, 10) // Adjust this value to increase the height
+                        
+                        HStack {
+                            Image(systemName: "crown")
+                                .foregroundColor(.brown)
+                            Text("開啟 SwiftiDate Supreme 會員標誌")
+                            Spacer()
+                            Toggle("", isOn: .constant(true))
+                                .labelsHidden()
+                        }
+                        .padding(.vertical, 10) // Adjust this value to increase the height
+                        
+                        HStack {
+                            Image(systemName: "envelope.fill")
+                                .foregroundColor(.pink)
+                            Text("邀請好友一起玩SwiftiDate")
+                            Spacer()
+                        }
+                        .padding(.vertical, 10) // Adjust this value to increase the height
+                        
+                        HStack {
+                            Image(systemName: "crown.fill")
+                                .foregroundColor(.pink)
+                            Text("兌換會員")
+                            Spacer()
+                        }
+                        .padding(.vertical, 10) // Adjust this value to increase the height
+                        
+                        HStack {
+                            Image(systemName: "person.fill")
+                                .foregroundColor(.cyan)
+                            Text("個人資料")
+                            Spacer()
+                        }
+                        .padding(.vertical, 10) // Adjust this value to increase the height
+                    }
+                    
+                    Section {
+                        HStack {
+                            Text("客服")
+                                .padding(.vertical, 10) // Adjust this value to increase the height
+                            Spacer()
+                            Image(systemName: "chevron.right")
+                                .foregroundColor(.gray) // Optional: Set the color of the chevron
+                        }
+
+                        Button(action: {
+                            isHelpView = true // Show HelpView
+                        }) {
+                            HStack {
+                                Text("幫助")
+                                    .padding(.vertical, 10)
+                                Spacer()
+                                Image(systemName: "chevron.right")
+                                    .foregroundColor(.gray)
+                            }
+                            .foregroundColor(.black) // Keep the text color unchanged when the button is tapped
+                        }
+                        
+                        HStack {
+                            Text("語言")
+                                .padding(.vertical, 10)
+                            Spacer()
+                            Image(systemName: "chevron.right")
+                                .foregroundColor(.gray)
+                        }
+
+                        HStack {
+                            Text("社區規範")
+                                .padding(.vertical, 10)
+                            Spacer()
+                            Image(systemName: "chevron.right")
+                                .foregroundColor(.gray)
+                        }
+
+                        HStack {
+                            Text("隱私權政策")
+                                .padding(.vertical, 10)
+                            Spacer()
+                            Image(systemName: "chevron.right")
+                                .foregroundColor(.gray)
+                        }
+
+                        HStack {
+                            Text("服務協議")
+                                .padding(.vertical, 10)
+                            Spacer()
+                            Image(systemName: "chevron.right")
+                                .foregroundColor(.gray)
+                        }
+
+                        HStack {
+                            Text("數據管理")
+                                .padding(.vertical, 10)
+                            Spacer()
+                            Image(systemName: "chevron.right")
+                                .foregroundColor(.gray)
+                        }
+
+                        HStack {
+                            Text("資料恢復")
+                                .padding(.vertical, 10)
+                            Spacer()
+                            Image(systemName: "chevron.right")
+                                .foregroundColor(.gray)
+                        }
+
+                        HStack {
+                            Text("檢查版本更新")
+                                .padding(.vertical, 10)
+                            Spacer()
+                            Image(systemName: "chevron.right")
+                                .foregroundColor(.gray)
+                        }
+
+                        HStack {
+                            Text("戀愛性格測驗")
+                                .padding(.vertical, 10)
+                            Spacer()
+                            Image(systemName: "chevron.right")
+                                .foregroundColor(.gray)
+                        }
+                    }
+                    
+                    Section {
+                        Text("登出")
+                            .padding(.vertical, 10) // Adjust this value to increase the height
+                            .frame(maxWidth: .infinity, alignment: .leading) // Extend Text to the full width
+                            .background(Color.white) // Make sure the background is set to white
+                            .foregroundColor(.red) // Set the text color to red
+                    }
+                    
+                    Section {
+                        Text("關閉帳號")
+                            .padding(.vertical, 10) // Adjust this value to increase the height
+                            .frame(maxWidth: .infinity, alignment: .leading) // Extend Text to the full width
+                            .background(Color.white) // Make sure the background is set to white
+                    }
                 }
             }
         }
+    }
+}
+
+struct SettingsView_Previews: PreviewProvider {
+    @State static var showSettingsView = true // Provide a sample state variable for the preview
+
+    static var previews: some View {
+        SettingsView(showSettingsView: $showSettingsView) // Use the binding variable in the preview
     }
 }
