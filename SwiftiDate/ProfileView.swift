bf5fa1b329fe8681dcf52f0ec5e15ce391c6ee82
stevenyu253436 <stevenyu.supreme@gmail.com>
2024-10-16 23:28:03 +0800

    feat: Replace 'photo1' with  and add placeholder logic
    - Updated the image display logic to use  if available, falling back to a placeholder image if not.
    - Added error handling to check for nil values and display a default placeholder image when  is unavailable.
    - Applied consistent styling for the image overlay.

--- SwiftiDate/ProfileView.swift
+++ SwiftiDate/ProfileView.swift
@@ -147,30 +147,84 @@ struct ProfileView: View {
                                 isShowingTurboView = false // Close the fullScreenCover when back button is pressed
                             })
                         }
+                        
+                        if userSettings.isProfilePhotoVerified {
+                            // 成就进度条
+                            VStack {
+                                HStack {
+                                    Text("打敗 \(String(format: "%.1f", userRankPercentage))%")
+                                        .font(.subheadline)
+                                        .fontWeight(.bold)
+                                    
+                                    // Add the question mark icon with tap gesture
+                                    Image(systemName: "questionmark.circle")
+                                        .resizable()
+                                        .frame(width: 20, height: 20)
+                                        .foregroundColor(.green)
+                                        .onTapGesture {
+                                            isShowingInfoPopup = true // Show the popup when tapped
+                                        }
+                                }
+                                .padding(.bottom, 5)
 
-                        // 成就进度条
-                        VStack {
+                                ProgressView(value: userRankPercentage / 100) // Convert the percentage to a fraction
+                                    .progressViewStyle(LinearProgressViewStyle(tint: .green))
+                                    .scaleEffect(x: 1, y: 4, anchor: .center)
+                            }
+                            .padding(.horizontal)
+                        } else {
+                            // 當頭像未通過驗證時，顯示提示視圖
                             HStack {
-                                Text("打敗 \(String(format: "%.1f", userRankPercentage))%")
+                                ZStack {
+                                    if let firstPhotoName = photos.first, let image = loadImageFromLocalStorage(named: firstPhotoName) {
+                                        Image(uiImage: image)
+                                            .resizable()
+                                            .frame(width: 50, height: 50)
+                                            .clipShape(Circle())
+                                            .overlay(
+                                                Circle().stroke(Color.white, lineWidth: 2)
+                                            )
+                                    } else {
+                                        // 顯示預設的佔位符圖片
+                                        Image(systemName: "person.crop.circle.fill")
+                                            .resizable()
+                                            .frame(width: 50, height: 50)
+                                            .foregroundColor(.gray)
+                                            .clipShape(Circle())
+                                            .overlay(
+                                                Circle().stroke(Color.white, lineWidth: 2)
+                                            )
+                                    }
+                                    
+                                    // 顯示 xmark，在圖像的上方
+                                    Circle()
+                                        .fill(Color.red)
+                                        .frame(width: 20, height: 20)
+                                        .overlay(
+                                            Image(systemName: "xmark")
+                                                .foregroundColor(.white)
+                                        )
+                                        .offset(x: 20, y: 20) // 調整 x 和 y 的偏移量，使其位於圖像上方
+                                }
+                                
+                                Text("請更換頭像照片後再查看喜歡數排名")
                                     .font(.subheadline)
                                     .fontWeight(.bold)
+                                    .foregroundColor(.white)
+                                    .padding(.leading, 10)
                                 
-                                // Add the question mark icon with tap gesture
-                                Image(systemName: "questionmark.circle")
+                                Spacer()
+                                
+                                Image(systemName: "lock.fill")
                                     .resizable()
-                                    .frame(width: 20, height: 20)
-                                    .foregroundColor(.green)
-                                    .onTapGesture {
-                                        isShowingInfoPopup = true // Show the popup when tapped
-                                    }
+                                    .frame(width: 24, height: 30)
+                                    .foregroundColor(.white)
                             }
-                            .padding(.bottom, 5)
-
-                            ProgressView(value: userRankPercentage / 100) // Convert the percentage to a fraction
-                                .progressViewStyle(LinearProgressViewStyle(tint: .green))
-                                .scaleEffect(x: 1, y: 4, anchor: .center)
+                            .padding()
+                            .background(Color.green)
+                            .cornerRadius(10)
+                            .padding(.horizontal)
                         }
-                        .padding(.horizontal)
                         
                         // Turbo, Crush, 讚美
                         HStack(spacing: 20) {
